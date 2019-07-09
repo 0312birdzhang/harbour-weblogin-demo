a0f7aaedeed8025e0bc75bcebc14861931d030d6
BirdZhang <0312birdzhang@gmail.com>
2019-07-09 18:26:04 +0800

    一些接口添加

--- qml/python/app.py
+++ qml/python/app.py
@@ -38,9 +38,19 @@ from bs4 import BeautifulSoup
 from diskcache import Cache
 
 DOMAIN_NAME = 'v2ex.com'
-BASE_URL = 'https://%s' % (DOMAIN_NAME, )
-TIMEZONE = timezone(timedelta(hours=2), 'Europe/Helsinki')
 HARBOUR_APP_NAME = 'harbour-weblogin-demo'
+BASE_URL = 'https://%s' % (DOMAIN_NAME, )
+
+# some api from https://github.com/greatyao/v2ex-android/blob/v2ex2/app/src/main/java/com/yaoyumeng/v2ex2/api/V2EXManager.java
+API_TOPIC_HOT = "api/topics/hot.json"
+API_TOPIC_LATEST = "api/topics/latest.json"
+API_REPLIES = "api/replies/show.json"
+API_TOPIC = "api/topics/show.json"
+API_ALL_NODE = "api/nodes/all.json"
+API_NODES_SHOW = "api/nodes/show.json"
+API_MEMBERS_SHOW = "api/members/show.json"
+
+TIMEZONE = timezone(timedelta(hours=2), 'Europe/Helsinki')
 HOME = os.path.expanduser('~')
 XDG_DATA_HOME = os.environ.get('XDG_DATA_HOME', os.path.join(HOME, '.local', 'share'))
 XDG_CONFIG_HOME = os.environ.get('XDG_CONFIG_HOME', os.path.join(HOME, '.config'))
@@ -51,6 +61,8 @@ CACHE_PATH = os.path.join(XDG_CACHE_HOME, HARBOUR_APP_NAME, HARBOUR_APP_NAME)
 class Api:
     def __init__(self):
         self.userId = 0
+        self.sessionId = ""
+        self.jar = requests.cookies.RequestsCookieJar()
         try:
             if not os.path.exists(CACHE_PATH):
                 os.makedirs(CACHE_PATH)
@@ -65,9 +77,12 @@ class Api:
         """
         Utils.log("start get other param")
         bduss = self.get_session_id_from_cookie()
-        uid, avatarUrl = self.get_user_id_from_username(username)
+        user_info = self.get_user_info_from_username(username)
         user = {}
-        if bduss and uid:
+        if bduss and user_info:
+            
+            uid = user_info.get("id")
+            avatarUrl = "http:%s" % (user_info.get("avatar_normal"), )
             user['uid'] = uid
             user['bduss'] = bduss
             user['username'] = username
@@ -99,23 +114,20 @@ class Api:
                 if cookie_name == 'A2':
                     return morsel.value
     
-    def get_user_id_from_username(self, username):
+    def get_user_info_from_username(self, username):
         Utils.log(username)
         """
         Get user_id from username
         """
-        user_home_url = "%s/api/members/show.json?username=%s" % (BASE_URL, username )
+        user_home_url = "%s/%s?username=%s" % (BASE_URL, API_MEMBERS_SHOW, username )
         try:
             r = requests.get(user_home_url, timeout = 10)
             user_info = r.json()
-            uid = user_info.get("id")
-            avatarUrl = user_info.get("avatar_normal")
-            if avatarUrl.startswith("//"):
-                avatarUrl = "http:" + avatarUrl
-            return uid, avatarUrl
+            return user_info
         except:
             Utils.log(traceback.format_exc())
             utils.error('Could not login. Please try again.')
+
 
     def set_logged_in_user(self, user={}):
         """
@@ -131,6 +143,8 @@ class Api:
                     self.cache.set('user.avatarUrl', user['avatarUrl'])
                 if 'bduss' in user:
                     self.cache.set('user.bduss', user['bduss'])
+                    self.sessionId = user['bduss']
+                    self.jar.set('A2', self.sessionId, domain='.v2ex.com', path='/')
                 return True
             else:
                 raise Exception('Could not save data.')
@@ -154,6 +168,30 @@ class Api:
                 user['avatarUrl'] = self.cache.get('user.avatarUrl')
                 return user
 
+    def get_topic_host(self):
+        """
+        Get hot topic
+        """
+        url = "%s/%s" % (BASE_URL, API_TOPIC_HOT)
+        try:
+            r = requests.get(url, timeout = 15)
+            return r.json()
+        except:
+            Utils.log(traceback.format_exc())
+            utils.error('Could not get hot topic. Please try again.')
+
+    def get_topic_latest(self):
+        """
+        Get latest topic
+        """
+        url = "%s/%s" % (BASE_URL, API_TOPIC_LATEST)
+        try:
+            r = requests.get(url, timeout = 15)
+            return r.json()
+        except:
+            Utils.log(traceback.format_exc())
+            utils.error('Could not get latest topic. Please try again.')
+
     def do_logout(self):
         """
         Logout by clear cookie
@@ -161,6 +199,7 @@ class Api:
 
         self.sessionId = ''
         self.userId = 0
+        self.jar = requests.cookies.RequestsCookieJar()
 
         # Clear WebKit cookies DB
         if os.path.exists(COOKIE_PATH):
